614a68da9415121f749be02892098a1aa28b05b1
Emmanuel Bronyah <emmanuelbronyah7@gmail.com>
2026-04-06 23:13:50 +0000

    Added fixtures to load user data

--- api/models.py
+++ api/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class User(models.Model):
 
     class Role(models.TextChoices):
-        ADMIN = "admin"
-        VENDOR = "vendor"
-        CUSTOMER = "customer"
+        ADMIN = "admin", "Admin"
+        VENDOR = "vendor", "Vendor"
+        CUSTOMER = "customer", "Customer"
 
     name = models.CharField(max_length=125)
-    role = models.CharField(max_length=10, choices=Role)
+    role = models.CharField(max_length=10, choices=Role.choices)
 
     def __str__(self):
         return f"Name: {self.name} Role: {self.role}"
